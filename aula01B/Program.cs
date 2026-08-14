ec60d58c77d02abf7ace54131099f79df3887e19
Neimar <neimaroliveiraq1@gmail.com>
2026-08-13 22:26:42 -0300

    feat : revisao de logica

--- aula01B/Program.cs
+++ aula01B/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+int anosAutonomia = 450;
+double capacidadeBateria = 75.5;
+string modeloVeiculo = "Byd";
+bool carregamentoRapido = true;
+
+Console.WriteLine($"Veiculo: {modeloVeiculo}");
+Console.WriteLine($"Autonomia:{anosAutonomia} km | Bateria: {capacidadeBateria}");
+Console.WriteLine($"Possui Carregamento Rapido: {carregamentoRapido}");
+
+//preciso que faça o calculo para saber quando vou pagar de imposto do carro
+const double Juros = 0.10; //10%
+double precoVeiculo = 20000.00;
+
+double valorImposto = precoVeiculo * Juros;
+Console.WriteLine($"O valor do Imposto é R$: {valorImposto:N2}");
+
